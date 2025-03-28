2fb72341bb569dc3985737a627c9c563625fe84b
LuigiRac <lui.rac06@gmail.com>
2025-03-28 12:55:16 +0100

    milestone 1

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+if(isset($_GET["password"])) {
+
+
+$maiuscole = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+$minuscole = "abcdefghijklmnopqrstuvwxyz";
+$numeri = 1234567890;
+$simboli = "!?£$%&/()=";
+
+
+$password = "";
+
+$caratteri = $maiuscole . $minuscole . $numeri . $simboli;
+
+for ($i=0; $i < $_GET['password']; $i++) { 
+    $posizioni_caratteri = rand(0, strlen($caratteri) -1);
+
+     $caratteri_casuali =  substr($caratteri, $posizioni_caratteri, 1);
+
+     $password .= $caratteri_casuali;
+}
+
+};
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Password-generator</title>
+</head>
+<body>
+
+<h3>GENERA PASSWORD</h3>
+<form class="form-inline d-flex">
+  <div class="form-group mx-sm-3 mb-2">
+    <input class="form-control" type="" id="password" name="password" min="5" max="20"  placeholder="Password">
+    
+
+  </div>
+  <div class="container">
+  <button class="btn btn-primary mb-2">Genera password</button>
+  </div>
+
+  
+</form>
+
+
+
+<?php
+if($password!= "") {
+
+?>
+<div class="mx-sm-3 mb-2">
+    <h3>La tua password è: </h3>
+   <div class="form-control min-width-auto"> <?php echo $password ?></div> 
+</div>
+
+<?php
+
+}
+?>
+</body>
+</html>
